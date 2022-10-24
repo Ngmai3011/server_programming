362f527fec0ebcc7a9e54f2ca6ce034182ec7ef1
Ngmai3011 <67302195+Ngmai3011@users.noreply.github.com>
2022-10-24 22:16:17 +0300

    Delete src directory

--- src/main/java/project/Bookstore/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package project.Bookstore.repository;
-
-import project.Bookstore.entity.User;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
-}
